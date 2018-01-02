17d2738500392fabfab4b5795bb727d057594f87
flored27 <dflores@fandm.edu>
2018-01-02 11:31:34 -0500

    Included an Ingredients Form, with ing name and prop amount

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,17 @@ const Form = (props) => {
   return (
   <div align="right" style={{padding:10 }} >
     <form onSubmit={props.handleSubmit}>
+      <h3>Cocktail:</h3>
       Name: <input name="name" value={props.formState.cocktail.name} onChange={(event)=>{props.handleCocktailChange(event)}} type="text"></input>
       <br />
       Description: <textarea name="description" value={props.formState.cocktail.description} onChange={(event)=>{props.handleCocktailChange(event)}} type="text"></textarea>
       <br />
       Instructions: <textarea name="instructions" value={props.formState.cocktail.instructions} onChange={(event)=>{props.handleCocktailChange(event)}} type="text"></textarea>
       <br />
-      <h3>Proportions:</h3>
+      <h3>Ingredients:</h3>
+      Name:  <input name="name" value={props.formState.ingredient.name} onChange={(event)=>{props.handleIngredientChange(event)}} type="text"></input>
+      Amount: <input name="amount" value={props.formState.proportion.amount} onChange={(event)=>{props.handleProportionChange(event)}} type="text"></input>
+
 
       <button type="submit">Submit</button>
     </form>
